9241d1900766255235dbe5936dcd5402aa46dc7e
gowthamr56 <gowtham180502@gmail.com>
2023-04-04 13:04:45 +0530

    Added CC, BCC, & Command Line Interface functionality

--- automate_emails.py
+++ automate_emails.py
@@ -2,20 +2,31 @@
 
 # SCRIPT TO AUTOMATE EMAILS
 
-import smtplib
 from dotenv import load_dotenv
 import os
 import time
-import ssl
 import sys
 from read_glsheets import read_glsheets
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import yagmail
+import argparse
 
 start_time = time.perf_counter()
 
 # load `.env` file
 load_dotenv()
+
+description = """\
+This project is a clone of Mail Merge which is one of the extensions in google sheets using Python.
+"""
+
+parser = argparse.ArgumentParser(description=description)
+
+# defining arguments for CLI
+parser.add_argument("link", help="requires google sheet public url")
+parser.add_argument("-cc", help="requires email addresses for Carbon Copy (CC)", nargs="+")
+parser.add_argument("-bcc", help="requires email addresses for Blind Carbon Copy (BCC)", nargs="+")
+
+args = parser.parse_args()
 
 HOST: str = "smtp.gmail.com"
 PORT: int = 465
@@ -23,50 +34,40 @@ PORT: int = 465
 login_email = os.getenv("login_email")
 login_passwd = os.getenv("login_passwd")  # use `App Passwords` option in gmail (If 2 factor auth is enabled)
 
+cc = args.cc
+bcc = args.bcc
+
+subject = "GOOGLE MAIL MERGE CLONE"
+
 # reading google sheet data using link
 link = sys.argv[1]
 records = read_glsheets(link)
+
+# initialization
+smtp = yagmail.SMTP(user=login_email, password=login_passwd, host=HOST, port=PORT)
 
 for index, record in enumerate(records, start=1):
 	first_name = record.get("First Name")
 	last_name = record.get("Last Name")
 	receiver_email = record.get("Receiver")
 
-	msg = MIMEMultipart("alternative")
-
-	msg["Subject"] = "Automating Emails with Python Script - GOOGLE MAIL MERGE CLONE"
-	
-	html_body = f"""\
+	message_body = f"""\
 	<html>
 	<body>
-		<h2>Hai {first_name} {last_name}</h2>
+		<h3>Hai {first_name} {last_name}</h3>
 		<p>I am a Gowtham. I am a curious Python Programmer, Blogger. I write blogs on <a href='medium.com/@gowtham180502'>medium</a> occasionally</p>
 		<p>To know more about me, then checkout <a href='gowtham.streamlit.app'>here</a>.</p>
 	</body>
 	</html>
 	"""
-	content = MIMEText(html_body, "html")
 
-	msg.attach(content)
-
-	# add a layer of security to the email that we sent
-	context = ssl.create_default_context()
-
-	with smtplib.SMTP_SSL(HOST, PORT, context=context) as smtp:
-		"""
-		# This docsting code will be helpful, when the ssl is not used
-
-		smtp.ehlo()  # this will call automatically in the background. But, even I explicitly calling here
-		smtp.starttls()  # adding encryption to the n/w traffic
-		smtp.ehlo()
-		"""
-
-		smtp.login(login_email, login_passwd)
-	
-		smtp.sendmail(login_email, receiver_email, msg.as_string())
+	smtp.send(to=receiver_email, cc=cc, bcc=bcc, subject=subject, contents=message_body)
 
 	os.system("clear") if sys.platform == "linux" else os.system("cls")
 	print(f"Progress: {index} of {len(records)} emails is sent")
+
+# to close the SMTP server connection 
+smtp.close()
 
 end_time = time.perf_counter()
 
